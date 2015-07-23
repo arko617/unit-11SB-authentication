9567f6b94bc54d14b9270a3041e018529559ee22
Arko Dewri <arkox617@gmail.com>
2015-07-23 09:20:46 -0700

    authentication done

--- client/js/app/main.js
+++ client/js/app/main.js
@@ -5,5 +5,38 @@ angular
 SignUpController.$inject = ['$http'];
 
 function SignUpController($http) {
-  this.newUser = {};
+  this.signup = false;
+  this.signin = false;
+
+  this.create = function(){
+	this.signup = true;
+	this.signin = false;  	
+  }
+
+  this.login = function(){
+  	this.signup = false;
+	this.signin = true;
+  }
+
+  this.username = "";
+  this.password = "";
+
+  this.newUsername = "";
+  this.newPassword = "";
+
+  this.register = function(){
+  	$http.post('/users', {
+  		username: this.newUsername, 
+  		password: this.newPassword
+  	}).success(function(data){
+  			console.log(data);
+  		});
+  }
+
+  this.getin = function(){
+  	$http.post('/login', {username: this.username, password: this.password}).
+  		success(function(data){
+  			console.log(data);
+  		});
+  }
 }
--- index.js
+++ index.js
@@ -16,30 +16,54 @@ var UserSchema = new Schema({
   password: { type: String, required: true},
   access_token: String
 });
+
 var DogSchema = new Schema({
   name: String,
   age: Number
 });
+
 var User = mongoose.model('User', UserSchema);
 var Dog = mongoose.model('Dog', UserSchema);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/', express.static('public'));
+app.use('/', express.static('client'));
 
 app.post('/users', function(req, res, next) {
-
+  var token = hat();
+  req.body['access_token'] = token;
+  console.log(req.body);
+  User.create(req.body, function(err,data){
+    if(err) return next(err);
+    res.end('done');
+  });
+  //console.log(User);
 });
-app.post('/login', function(req, res) {
 
+app.post('/login', function(req, res) {
+  console.log(req.body);
+  User.findOne(req.body, function(err, data){
+    if(err) return next(err);
+    // res.cookie('access_token', data.access_token, {maxAge: 360000, httpOnly: true});
+    app.get('/dogs', function (req, res, next) {
+      // console.log(req.cookies.access_token);
+      // User.findOne({'access_token': req.cookies.access_token}
+      if(err) return next(err);
+      
+      if(data)
+        res.end("WELCOME TO DOGS");  
+    });
+
+    res.end('done');
+
+  });
 
 });
 
 // the dogs should not be shown unless the user is logged in
 //
-app.get('/dogs', function (req, res, next) {
-});
+
 
 app.listen(3000)
 
